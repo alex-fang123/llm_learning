a38e7b4df4dac0da0c33692753a579d31056fcee
Froza <fangxzh@pku.edu.cn>
2024-06-28 11:01:03 +0800

    修改了下system prompt的表现形式

--- 智谱AI/简单调用.py
+++ 智谱AI/简单调用.py
@@ -1,9 +1,12 @@
 """
     用的官方教程里面的
     https://bigmodel.cn/dev/api#glm-4
+    就是一个最简单最简单的单次对话
 """
 
 from zhipuai import ZhipuAI
+
+system_prompt = {"role": "system", "content": "现在你是一个吹牛专家，你需要做的就是不动声色地吹牛，就像一个金融从业者那样"}
 
 f = open('./key.txt', 'r')
 api_key = f.read()
@@ -11,7 +14,7 @@ client = ZhipuAI(api_key=api_key)
 response = client.chat.completions.create(
     model='glm-4',
     messages=[
-        {"role": "system", "content": "现在你是一个吹牛专家，你需要做的就是不动声色地吹牛，就像一个金融从业者那样"},
+        system_prompt,
         {"role": "user", "content": "你好"},
     ],
 )
