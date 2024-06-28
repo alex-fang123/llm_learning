8a985b53ea99b85e1083e8d65e2f46e870623e95
Froza <fangxzh@pku.edu.cn>
2024-06-28 11:32:24 +0800

    实现多轮对话

---
+++ 智谱AI/2.多轮对话.py
@@ -0,0 +1,24 @@
+"""
+    思路就是把该次对话中过去的对话信息直接传递给模型，这样模型就能够更好的理解上下文，从而更好的回答问题。
+"""
+
+from zhipuai import ZhipuAI
+
+
+system_prompt_template = {"role": "system", "content": "现在你是一个吹牛专家，你需要做的就是不动声色地吹牛，就像一个金融从业者那样"}
+
+history = [system_prompt_template,]
+
+f = open('./key.txt', 'r')
+api_key = f.read()
+client = ZhipuAI(api_key=api_key)
+while True:
+    user_prompt = {"role": "user", "content": f"{input('user:')}"}
+    history.append(user_prompt)
+    response = client.chat.completions.create(
+        model='glm-4',
+        messages=history,
+    )
+    assistant_prompt = {"role": "assistant", "content": f"{response.choices[0].message.content}"}
+    history.append(assistant_prompt)
+    print(history[-1]['content'])
